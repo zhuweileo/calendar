69af2a02e9046c3a4d3f7f1f3dd90806cff08f60
zhuwei <zhuwei@koolearn-inc.com>
2019-10-12 19:55:46 +0800

    feat: 写table

---
+++ src/modules/Calender/Calender.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {Paper, Table, TableBody, TableHead, TableRow, TableCell} from "@material-ui/core";
+
+type WeekDay = 'sun' | 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat';
+
+interface TheadItem {
+    key: WeekDay,
+    title: string,
+}
+
+type TrowItem = {
+    [key in WeekDay]: any
+}
+const theadList: TheadItem[] = [
+    {key: 'sun', title: '星期天'},
+    {key: 'mon', title: '星期一'},
+    {key: 'tue', title: '星期二'},
+    {key: 'wed', title: '星期三'},
+    {key: 'thu', title: '星期四'},
+    {key: 'fri', title: '星期五'},
+    {key: 'sat', title: '星期六'},
+];
+
+const dataSource: TrowItem[] = [
+    {sun: '111', mon: '1', tue: '', wed: '', thu: '', fri: '', sat: ''},
+    {sun: '111', mon: '1', tue: '', wed: '', thu: '', fri: '', sat: ''},
+    {sun: '111', mon: '1', tue: '', wed: '', thu: '', fri: '', sat: ''},
+    {sun: '111', mon: '1', tue: '', wed: '', thu: '', fri: '', sat: ''},
+    {sun: '111', mon: '1', tue: '', wed: '', thu: '', fri: '', sat: ''},
+];
+
+export default function Calender() {
+    return <div>
+        <Paper>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        {
+                            theadList.map(day => <TableCell>{day}</TableCell>)
+                        }
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </Paper>
+    </div>
+}
---
+++ src/modules/RangePicker/RangePicker.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+interface Props {
+
+}
+export default function RangePicker(props: Props) {
+    return <div className={'rang-picker'}></div>
+}
