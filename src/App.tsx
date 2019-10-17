7fc32d8c66df2e96645ce55841d4556ffc0638db
zhuwei <zhuwei_leo@163.com>
2019-10-17 23:26:07 +0800

    feat: 调整组价结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Calendar from "./modules/Calendar/Calendar";
+import MDialog from "./modules/MDialog/MDialog";
+import {Paper} from "@material-ui/core";
 
 const App: React.FC = () => {
+    const [open,setOpen] = useState(false);
+    function handleClose() {
+        setOpen(false);
+    }
+
+    function itemInner(item: any) {
+        return <div onClick={() => setOpen(true)}>{item}</div>
+    }
+
     return (
         <div className="App">
-            <Calendar></Calendar>
+            <Calendar itemRender={itemInner} ></Calendar>
+            <MDialog open={open} onClose={handleClose} selectedValue={''}></MDialog>
         </div>
     );
 };
