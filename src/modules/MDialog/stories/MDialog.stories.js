dfa639abe1dd6a3464eeff85ccbf659f680371c5
zhuwei <zhuwei@koolearn-inc.com>
2019-11-04 20:30:49 +0800

    feat: 添加storybook

---
+++ src/modules/MDialog/stories/MDialog.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+
+export default {
+    title: '弹窗',
+}
+export const story = () => <div>ceshi</div>
+export const story1 = () => <div>ceshi</div>
