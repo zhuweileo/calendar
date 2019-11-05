291ad013df1b2fd5378bf27f46809270a03e0073
zhuwei <zhuwei@koolearn-inc.com>
2019-11-05 16:11:33 +0800

    feat: 完成时间段规则

--- src/modules/MDialog/MDialog.tsx
+++ src/modules/MDialog/MDialog.tsx
@@ -1,60 +1,73 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {Props} from "./MDialog.d";
-import {Dialog,} from "@material-ui/core";
+import {Checkbox, Dialog, FormControlLabel} from "@material-ui/core";
 import className from 'classnames/bind';
 import style from './MDialog.module.scss';
 import DateFnsUtils from '@date-io/date-fns';
 import 'date-fns'
-import {
-    MuiPickersUtilsProvider,
-    KeyboardTimePicker,
-} from '@material-ui/pickers';
+import {KeyboardTimePicker, MuiPickersUtilsProvider,} from '@material-ui/pickers';
 
 const cx = className.bind(style);
 
 export default function MDialog(props: Props) {
     const {
         open,
-        onClose = () => {},
+        onClose = () => {
+        },
         selectedValue = {}
     } = props;
 
-    const {tableDate} = selectedValue;
+    const {sleepTime, tableDate} = selectedValue;
 
-    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(sleepTime || new Date());
+    const [isNextDay, setIsNextDay] = useState(false);
+    const [color, setColor] = useState('#ccc');
+    const isFirstRef = useRef(true);
+
+    useEffect(function () {
+        if(isFirstRef.current && !sleepTime) {
+            isFirstRef.current = false;
+            return
+        }
+
+        function getColorByTime() {
+            const curTime = new Date(selectedDate);
+            const curHour = curTime.getHours();
+            const curMinute = curTime.getMinutes();
+
+            if (!isNextDay && curHour < 23) return 'green';
+            if (!isNextDay && curHour === 23 && curMinute > 0) return 'yellow';
+            return 'red';
+        }
+
+        const color = getColorByTime();
+        setColor(color);
+
+    }, [ selectedDate, isNextDay, sleepTime]);
 
     function closeHandler() {
         onClose()
     }
 
-    const handleDateChange = (date: any) => {
-        // console.log(date);
+    function handleDateChange(date: any) {
         setSelectedDate(date);
-    };
-
-    const curTime = new Date(selectedDate);
-    const curDate = curTime.getDate();
-    const curHour = curTime.getHours();
-    console.log(curDate);
-
-    function getColorByTime(){
-        if(!tableDate) return '#ccc';
-        if(curDate === tableDate &&  curHour < 23 ) {
-            return 'green'
-        }
-        // if(curDate)
     }
 
-    const timeBg = {
-        color: [],
+    function onCheckChange(e: any) {
+        setIsNextDay(e.target.checked)
+    }
+
+    const timeStyle = {
+        border: `1px solid ${color}`,
     };
 
     return (
         <Dialog open={open} onClose={closeHandler}>
             <div className={cx('m-dialog')}>
-                <div  className={cx('time-wrap')}>
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <div className={cx('time-wrap')} style={timeStyle}>
+                    <MuiPickersUtilsProvider  utils={DateFnsUtils}>
                         <KeyboardTimePicker
+                            className={cx('time-picker')}
                             margin="normal"
                             id="time-picker"
                             label="Time picker"
@@ -65,6 +78,9 @@ export default function MDialog(props: Props) {
                             }}
                         />
                     </MuiPickersUtilsProvider>
+                    <FormControlLabel control={
+                        <Checkbox checked={isNextDay} onChange={onCheckChange}/>
+                    } label={'第二天'}/>
                 </div>
             </div>
         </Dialog>
